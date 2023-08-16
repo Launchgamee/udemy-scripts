391176f143987ba67216f100909049d1a0c74b90
Launchgamee <samm04826@gmail.com>
2023-08-16 11:11:19 -0400

    first commit

---
+++ course.sql
@@ -0,0 +1,19 @@
+ ----------------------SECTION-2------------------------
+
+--CREATE DATABASE Udemy
+--DROP DATABASE Udemy
+--USE Udemy
+
+--*********************************************
+
+SELECT 2 + 1  As Result
+Go -- END a batch on exection 
+SELECT 3 * 2  As Result
+GO -- batch
+SELECT 1/0  As Result
+
+
+
+--**********SECTION-3 ----------------------
+
+0
